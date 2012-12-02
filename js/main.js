dbe7d8aaa8489a4ba967f8fadd7dbe0a2f92f994
dawsonloudon <dawson@dawsonloudon.com>
2012-12-02 06:36:56 -0800

    updates to fix new view change after login

--- js/main.js
+++ js/main.js
@@ -27,101 +27,69 @@
   });
 
   function changePage(page){
-	  if(currentPage != page) {
-		  $('.content_container').animate({opacity:0},500,function(){
-			  $('#'+currentPage).html($('.content_container').html());
-			  $('div.content_container').removeClass(currentPage);
-			  currentPage = page;
-			  $('div.content_container').html($('#'+page).html()).addClass(page);
-			  $('.content_container').delay(250).animate({opacity:1},500,function(){
-			    if(page == 'home'){
-    $('#loginbtn').live('touchstart',function(){
-      authUser();
-    });
-			    	$('#loginusername').focus(function(){
-					if($(this).val() == 'username'){
-						$(this).val('');
-						$(this).blur(function(){
-							if($(this).val() == ''){
- 								$(this).val('username');
-							}
-						});
-      					}
-    				});
-    			    	/*$('#loginpassword').focus(function(){
-     	 				if($(this).val() == 'password'){
-        					$(this).val('');
-        					$(this).blur(function(){
-          						if($(this).val() == ''){
-            							$(this).val('password');
-          						}
-        					});
-      					}
-    			    	});*/
-			    }
-			    else if(page == 'signupform'){
-    $('#signupbtn').live('touchstart',function(){
-      createUser();
-    });
-			    	$('#signupusername').focus(function(){
-					if($(this).val() == 'Enter A Username'){
-						$(this).val('');
-						$(this).blur(function(){
-							if($(this).val() == ''){
- 								$(this).val('Enter A Username');
-							}
-						});
-      					}
-    				});
-			    	$('#signupemail1').focus(function(){
-					if($(this).val() == 'Enter Your Email'){
-						$(this).val('');
-						$(this).blur(function(){
-							if($(this).val() == ''){
- 								$(this).val('Enter Your Email');
-							}
-						});
-      					}
-    				});
-			    	$('#signupemail2').focus(function(){
-					if($(this).val() == 'Confirm Your Email'){
-						$(this).val('');
-						$(this).blur(function(){
-							if($(this).val() == ''){
- 								$(this).val('Confirm Your Email');
-							}
-						});
-      					}
-    				});
-			    	/*$('#password').focus(function(){
-					if($(this).val() == 'Enter Your Password'){
-						$(this).val('');
-						$(this).blur(function(){
-							if($(this).val() == ''){
- 								$(this).val('Enter Your Password');
-							}
-						});
-      					}
-    				});
-			    	$('#password2').focus(function(){
-					if($(this).val() == 'Confirm Your Password'){
-						$(this).val('');
-						$(this).blur(function(){
-							if($(this).val() == ''){
- 								$(this).val('Confirm Your Password');
-							}
-						});
-      					}
-    				});*/
-			    }
+    if(currentPage != page){
+      $('.content_container').animate({opacity:0},500,function(){
+        $('#'+currentPage).html($('.content_container').html());
+        $('div.content_container').removeClass(currentPage);
+        currentPage = page;
+        $('div.content_container').html($('#'+page).html()).addClass(page);
+        $('.content_container').delay(250).animate({opacity:1},500,function(){
+          if(page == 'home'){
+            $('#loginbtn').live('touchstart',function(){
+              authUser();
+            });
+            $('#loginusername').focus(function(){
+              if($(this).val() == 'username'){
+                $(this).val('');
+                $(this).blur(function(){
+                  if($(this).val() == ''){
+                    $(this).val('username');
+                  }
+                });
+              }
+            });
+          }
+          else if(page == 'signupform'){
+            $('#signupbtn').live('touchstart',function(){
+              createUser();
+            });
+            $('#signupusername').focus(function(){
+              if($(this).val() == 'Enter A Username'){
+                $(this).val('');
+                $(this).blur(function(){
+                  if($(this).val() == ''){
+                    $(this).val('Enter A Username');
+                  }
+                });
+              }
+            });
+            $('#signupemail1').focus(function(){
+              if($(this).val() == 'Enter Your Email'){
+                $(this).val('');
+                $(this).blur(function(){
+                  if($(this).val() == ''){
+                    $(this).val('Enter Your Email');
+                  }
+                });
+              }
+            });
+            $('#signupemail2').focus(function(){
+              if($(this).val() == 'Confirm Your Email'){
+                $(this).val('');
+                $(this).blur(function(){
+                  if($(this).val() == ''){
+                    $(this).val('Confirm Your Email');
+                  }
+                });
+              }
+            });
+          }
           else if(page == 'maps'){
             relocate();
           }
-			    // contentScroller = null;
-			    // contentScroller = new iScroll('scroller');
-			  });
-		  });
-	  }
+        });
+      });
+    }
   }
 
   function checkUser(){
