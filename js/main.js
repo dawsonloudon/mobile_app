faa0ac127e5fc4d0c3636fca33c0534f1abd6210
johnunderwood <john.underwood@noblestudios.com>
2012-12-01 17:13:20 -0800

    Added a changePage function to main.js which manipulates the containers in index.html for each of the pages and a main container with a header, footer, nav, and the article. Added basic css for the containers.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+var currentPage = '';
 
+$(document).ready(function(){
+	changePage('home');
+	$('nav a').bind('touchstart',function(){
+		changePage($(this).attr('data-page'));
+	});
+});
+
+function changePage(page){
+	if(currentPage != page) {
+		$('.content_container').animate({opacity:0},500,function(){
+			currentPage = page;
+			$('.content_loading_container').html($('#'+page);
+			$('.content_container').delay(250).animate({opacity:1},500);
+		});
+	}
+}
