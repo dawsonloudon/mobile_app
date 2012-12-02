6a36cd5b76ae141f3c17c6f337487873f347ba9d
johnunderwood <john.underwood@noblestudios.com>
2012-12-02 02:38:01 -0800

    Added in the basic UI for the Login Page, Signup Page, and a start to view the next screen(map/list)

--- js/main.js
+++ js/main.js
@@ -18,13 +18,9 @@
   }
 
   $(document).ready(function(){
-	  changePage('home');
-	  $('nav a:eq(0)').addClass('selected');
-	  $('nav a').bind('touchstart',function(){
-		  changePage($(this).attr('data-page'));
-		  $('nav a').removeClass('selected');
-		  $(this).addClass('selected');
-	  });
+    $('.nav').live('touchstart',function(){
+      changePage($(this).attr('data-page'));
+    });
     $('#loginbtn').live('touchstart',function(){
       authUser();
     });
@@ -39,11 +35,87 @@
   function changePage(page){
 	  if(currentPage != page) {
 		  $('.content_container').animate({opacity:0},500,function(){
+			  $('#'+currentPage).html($('.content_container').html());
+			  $('.content_container').removeClass(currentPage);
 			  currentPage = page;
-			  $('.content_container').html($('#'+page).html());
+			  $('.content_container').html($('#'+page).html()).addClass(page);
 			  $('.content_container').delay(250).animate({opacity:1},500,function(){
-				  contentScroller = null;
-				  contentScroller = new iScroll('scroller');
+			    if(page == 'home'){	  
+			    	$('#loginusername').live('touchstart',function(){
+					if($(this).value() == 'username'){
+						$(this).value('');
+						$(this).blur(function(){
+							if($(this).value() == ''){
+ 								$(this).value('username');
+							}
+						});
+      					}
+    				    });
+    			    	$('#loginpassword').live('touchstart',function(){
+     	 				if($(this).value() == 'password'){
+        					$(this).value('');
+        					$(this).blur(function(){
+          						if($(this).value() == ''){
+            							$(this).value('password');
+          						}
+        					});
+      					}
+    			    	});
+			    }
+			    else if(page == 'signupform'){
+			    	$('#signupusername').live('touchstart',function(){
+					if($(this).value() == 'Enter A Username'){
+						$(this).value('');
+						$(this).blur(function(){
+							if($(this).value() == ''){
+ 								$(this).value('Enter A Username');
+							}
+						});
+      					}
+    				});
+			    	$('#signupemail1').live('touchstart',function(){
+					if($(this).value() == 'Enter Your Email'){
+						$(this).value('');
+						$(this).blur(function(){
+							if($(this).value() == ''){
+ 								$(this).value('Enter Your Email');
+							}
+						});
+      					}
+    				});
+			    	$('#signupemail2').live('touchstart',function(){
+					if($(this).value() == 'Confirm Your Email'){
+						$(this).value('');
+						$(this).blur(function(){
+							if($(this).value() == ''){
+ 								$(this).value('Confirm Your Email');
+							}
+						});
+      					}
+    				});
+			    	$('#password').live('touchstart',function(){
+					if($(this).value() == 'Enter Your Password'){
+						$(this).value('');
+						$(this).blur(function(){
+							if($(this).value() == ''){
+ 								$(this).value('Enter Your Password');
+							}
+						});
+      					}
+    				});
+			    	$('#password2').live('touchstart',function(){
+					if($(this).value() == 'Confirm Your Password'){
+						$(this).value('');
+						$(this).blur(function(){
+							if($(this).value() == ''){
+ 								$(this).value('Confirm Your Password');
+							}
+						});
+      					}
+    				});
+			    }
+			    contentScroller = null;
+			    contentScroller = new iScroll('scroller');
 			  });
 		  });
 	  }
@@ -51,9 +123,15 @@
 
   function checkUser(){
     if(!user){
-      showLogin();
+      changePage('home');
     }
     else{
       changePage('maps');
     }
+  }
+
+  function noConnection(){
+    $('#loginform').css({'display':'none'});
+    $('#noconnection').css({'display':'block'});
+    changePage('home');
   }
